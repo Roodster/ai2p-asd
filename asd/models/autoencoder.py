85bdd6b26b6153c19d614669d7209f12c8e887d2
Rody Haket <rhaket@student.tudelft.nl>
2024-09-23 16:30:54 +0200

    Fixed shallow autoencoder logic

--- asd/models/autoencoder.py
+++ asd/models/autoencoder.py
@@ -5,8 +5,20 @@ from torch.utils.data import DataLoader
 
 from asd.models.base import BaseModel
 
+class ShallowEncoder(BaseModel):
+    def __init__(self, args, input_dim=256, hidden_dim=64):
+        super(ShallowEncoder, self).__init__(args)
+        
+        self.linear1 = nn.Linear(input_dim, hidden_dim, bias=True)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+    def forward(self, x):
+        batch_size, channels, seq_len, _ = x.shape
+        x = x.view(batch_size * channels * seq_len, -1)  # Reshape to 2D
+        x = self.leaky_relu(self.linear1(x))
+        return x
+
 class ShallowDecoder(BaseModel):
-    
     def __init__(self, args, input_dim, output_dim):
         super(ShallowDecoder, self).__init__(args)
         
@@ -17,38 +29,26 @@ class ShallowDecoder(BaseModel):
         x = self.leaky_relu(self.linear1(x))
         return x
 
-class ShallowEncoder(BaseModel):
-    
-    def __init__(self, args, input_dim=256, hidden_dim=64):
-        super(ShallowEncoder, self).__init__(args)
-        
-        self.linear1 = nn.Linear(input_dim, hidden_dim, bias=True)
-        self.leaky_relu = nn.LeakyReLU(0.2)
-
-
-    def forward(self, x):
-        x = self.leaky_relu(self.linear1(x))
-        return x
-                
-
 class ShallowAE(BaseModel):
-    
     def __init__(self, args, input_dim=256, hidden_dim=64):
         super(ShallowAE, self).__init__(args)
         
         self.encoder = ShallowEncoder(args, input_dim=input_dim, hidden_dim=hidden_dim)
         self.decoder = ShallowDecoder(args, input_dim=hidden_dim, output_dim=input_dim)
-        
     
     def forward(self, x):
         print(f'x1.shape {x.shape}')
+        original_shape = x.shape
+        
         x = self.encoder(x)
         print(f'x2.shape {x.shape}')
 
         x = self.decoder(x)
         print(f'x3.shape {x.shape}')
         
-        return x
+        # Reshape back to original shape
+        x = x.view(original_shape)
+        print(f'x4.shape {x.shape}')
     
 class SoftMaxClassifier(BaseModel):
     
