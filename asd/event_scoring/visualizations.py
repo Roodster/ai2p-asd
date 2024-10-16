2f29cd075e18f5e7c48fc5a15adc6c6a0645cb91
Parviz <P.ahmadov@student.tudelft.nl>
2024-10-16 12:04:35 +0200

    bug fix

--- asd/event_scoring/visualizations.py
+++ asd/event_scoring/visualizations.py
@@ -120,7 +120,7 @@ def plotIndividualEvents(ref: Annotation, hyp: Annotation,
     plt.figure(figsize=(16, nrow * 2))
     for i, window in enumerate(listofWindows):
         ax = plt.subplot(nrow, NCOL, i + 1)
-        plotEventScoring(ref.mask, hyp.mask, showLegend=False, ax=ax)
+        plotEventScoring(ref, hyp, showLegend=False, ax=ax)
         ax.set_xlim(window)
         plt.title('Event {}'.format(i))
     plt.tight_layout()
