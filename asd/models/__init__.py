f30fbdd48c23d88aee31c7c5ee7f6395e72e0f87
Parviz <P.ahmadov@student.tudelft.nl>
2024-10-16 03:46:09 +0200

    bug fix

--- asd/models/__init__.py
+++ asd/models/__init__.py
@@ -1,3 +1,4 @@
 from asd.models.autoencoder import BetaDecoder, BetaEncoder, BetaVAE, SoftMaxClassifier, ShallowAE, ShallowEncoder, ShallowDecoder
 from asd.models.transformer import VisionTransformer, SSLTransformer, CutRearrange, VisualAttentionModel
 from asd.models.model import CNNBiLSTM, Model
+from asd.models.DARLNet import DARLNet
