f62b8b46b16a7b27fbc5963ca02b76c9be9271a2
Rody Haket <rhaket@student.tudelft.nl>
2024-10-19 08:45:39 +0200

    Additional default model

--- asd/models/base.py
+++ asd/models/base.py
@@ -1,5 +1,5 @@
+import torch as th
 import torch.nn as nn
-
 
 class BaseModel(nn.Module):
     def __init__(self, args):
@@ -7,3 +7,24 @@ class BaseModel(nn.Module):
         self.args = args
         self.device = args.device
         self.to(args.device)
+        
+class DetectionModel(BaseModel):
+
+    def __init__(self, args, layers):
+        super(DetectionModel).__init__(args=args)
+        
+        if not isinstance(layers, list):
+            layers = [layers]
+            
+        self.layers = layers
+        
+    def forward(self, x):
+        
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+
+
+
+
